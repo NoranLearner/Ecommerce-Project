ebaebfd8d167f70a69e3db057dc82e6d73235ad8
NoranLearner <noranmostafa843@gmail.com>
2022-04-19 13:32:33 +0200

    Frontend Theme

--- routes/front.php
+++ routes/front.php
@@ -5,18 +5,18 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Front Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can register front routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
 */
 
-/* Route::get('/', function () {
-    return view('welcome');
-}); */
+Route::get('/', function () {
+    return view('front.home');
+});
 
 Auth::routes();
 
